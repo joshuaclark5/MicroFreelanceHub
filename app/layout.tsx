4b4a409adfa4abd675ae095f2f79cf2700861626
Josh Clark <joshuastevenclark@gmail.com>
2026-01-15 19:06:30 -0700

    Ignore node_modules and fix homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import '../styles/globals.css';
+import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'MicroFreelanceHub',
   description: 'Create clean and structured scopes of work fast',
 };
